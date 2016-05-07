b07825755c11fb4d7aa0d35e3e7481fb31d87055
hoofmen <hoofmen@gmail.com>
2016-05-07 00:17:06 -0700

    latest reading messages and posting one

--- app/src/main/java/com/hoofmen/mapchat/MapsActivity.java
+++ app/src/main/java/com/hoofmen/mapchat/MapsActivity.java
@@ -10,6 +10,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
@@ -18,23 +19,44 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements LocationListener {
 
     private GoogleMap mMap;
     private Location myLocation;
     private Marker marker;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +66,9 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
         setContentView(R.layout.activity_maps);
 
         setUpMapIfNeeded();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void setUpMapIfNeeded() {
@@ -70,7 +95,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
             mMap.setMyLocationEnabled(true);
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         } else {
-            System.out.println("no permission for Location!!!");
+            Toast.makeText(MapsActivity.this, "no permission for Location!!!", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -103,37 +128,124 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
 
         // Zoom in the Google Map
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-        marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet("I'm watching..."));
-        sendHttpGetVolley();
+        //talk to the server
+        getMessages();
+        sendMessage();
     }
 
-    private void sendHttpGetVolley(){
-        String url = "http://httpbin.org/html";
+    private void getMessages() {
+
+        //Map<String,Object> location = new HashMap<String,Object>();
+        //location.put("lat", myLocation.getLatitude());
+        //location.put("lon",  myLocation.getLongitude());
+        //List<Map<String,Object>> array = new ArrayList<Map<String, Object>>();
+        //array.add(location);
+        JSONArray ja = null;
+        try {
+            ja = new JSONArray("[]");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        String url = "http://winter-agility-399.appspot.com/get-messages";
 
         // Request a string response
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest stringRequest = new JsonArrayRequest(Request.Method.POST, url,ja,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-
+                    public void onResponse(JSONArray response) {
                         // Result handling
-                        String message = response.substring(0,15);
-                        System.out.println(message);
-                        Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
-
+                        System.out.println("GOT THIS MESSAGE:" + response.toString());
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject jsonObj = response.getJSONObject(i);
+                                System.out.println("GOT THIS MESSAGE:" + jsonObj.toString());
+                                marker = mMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(jsonObj.optJSONObject("location").optDouble("lat"), jsonObj.optJSONObject("location").optDouble("lon")))
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                                        .alpha(0.5f)
+                                        .title(jsonObj.optString("message")).snippet("duration=" + jsonObj.optInt("duration")));
+                            }
+                        } catch (JSONException ex){
+                            System.err.println(ex.toString());
+                        }
                     }
-                }, new Response.ErrorListener() {
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Error handling
+                        String message = "Something went wrong!";
+                        Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
+                        System.out.println(message);
+                        error.printStackTrace();
+                    }
+                }){
+
             @Override
-            public void onErrorResponse(VolleyError error) {
-
-                // Error handling
-                String message = "Something went wrong!";
-                Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
-                System.out.println(message);
-                error.printStackTrace();
-
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("X-AUTH-TOKEN", "thisisaverysecrettoken");
+                return headers;
             }
-        });
+        };
+
+        // Add the request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+
+    private void sendMessage() {
+
+        Map<String, Object> jsonParams = new HashMap<String, Object>();
+
+        jsonParams.put("message", "Calling from Android, yo yo yo !!!");
+        jsonParams.put("duration", 20);
+        Map<String,Object> location = new HashMap<String,Object>();
+        location.put("lat", myLocation.getLatitude());
+        location.put("lon",  myLocation.getLongitude());
+        jsonParams.put("location",  location);
+
+
+        String url = "http://winter-agility-399.appspot.com/put-message";
+
+        // Request a string response
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, url,new JSONObject(jsonParams),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Result handling
+                        String message = response.optString("message").toString();
+                        System.out.println("GOT THIS MESSAGE:" + response.toString());
+                        marker = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(response.optJSONObject("location").optDouble("lat"), response.optJSONObject("location").optDouble("lon")))
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                                .alpha(0.7f)
+                                .title(message).snippet("duration=" + response.optInt("duration")));
+                        Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Error handling
+                        String message = "Something went wrong!";
+                        Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
+                        System.out.println(message);
+                        error.printStackTrace();
+                    }
+                }){
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("X-AUTH-TOKEN", "thisisaverysecrettoken");
+                return headers;
+            }
+        };
 
         // Add the request to the queue
         Volley.newRequestQueue(this).add(stringRequest);
@@ -149,13 +261,15 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
 
         try {
             gps_enabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        } catch(Exception ex) {}
+        } catch (Exception ex) {
+        }
 
         try {
             network_enabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        } catch(Exception ex) {}
+        } catch (Exception ex) {
+        }
 
-        if(!gps_enabled && !network_enabled){
+        if (!gps_enabled && !network_enabled) {
             //notify user
             AlertDialog.Builder gpsAlert = new AlertDialog.Builder(this);
             gpsAlert.setMessage("Please enable location settings on your device.");
@@ -203,5 +317,45 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.hoofmen.mapchat/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.hoofmen.mapchat/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
